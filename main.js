37221b93c2fe2978102b62500ee96d17e1a7c6f9
Gabin Lefranc <gabin.lefranc@gmail.com>
2020-01-02 16:01:12 +0100

    Contenu rapide page web

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+function resetContent()
+{
+    var ctpages = document.getElementsByClassName("content-page")
+    Array.from(ctpages).forEach(ctpage => {
+        ctpage.classList.add('hidden')
+    });
+}
+function displayContent(which)
+{
+    resetContent()
+    var ctpage=document.getElementById(which);
+    ctpage.classList.remove("hidden")
+}
 document.addEventListener('DOMContentLoaded', function(){ 
     window.addEventListener("scroll", function(){
         var scrollTop = window.scrollY;
